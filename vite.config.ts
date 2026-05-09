c04c98a217abd12f2d9d02379a607b188b52f403
Aurora0529 <2946134765@qq.com>
2026-05-09 09:20:05 +0800

    修正 GitHub Pages 部署路径

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig(() => {
-  const base = process.env.VITE_BASE ?? '/ruc-time-station/'
+  const base = process.env.VITE_BASE ?? '/RUC-SGYZ/'
   return {
     base,
     build: {
